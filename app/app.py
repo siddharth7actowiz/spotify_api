d40d8bd56f1c41dbcbbb2d0074937c977c9d446a
Siddharth Doshi <Siddharth.Doshi@act.local>
2026-04-17 09:48:27 +0530

    Spotify Artist Data with Artist Name,Id<and Playlist_Url

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from curl_cffi import requests
+import requests
 from fastapi import FastAPI
 import json
 #Creating a FastAPI Instance
@@ -6,13 +6,14 @@ import json
 app=FastAPI()
 
 
+
 headers = {
     'accept': 'application/json',
     'accept-language': 'en',
     'app-platform': 'WebPlayer',
-    'authorization': 'Bearer BQBZbmfIHIyAPyBhW8AcbbO2TFmnU5Ut8Vf8Jstf28Bc1cBquDFUcTW8iBJocAidYqmBWVvDuSVvz1liVExzuKtamUk7ZyFwiSGh1fIQ4H0ww1JwMA7tvlpFHjATiBNCsrJO5bVu61o',
+    'authorization': 'Bearer BQDdFDGWbIXIp0CCPdIk8vKfDioD9gpM_8fzkeZ47VHsHCR5UxHJEAL5eaTMoS4WQ716x9JTRoj7CBgo0acI2B_-F4oL4I8J2Lk-8zQ9ZywBIwA0K7OIO-ZI13fBJlKy0Lv2ULAZjmw',
     'cache-control': 'no-cache',
-    'client-token': 'AAB38E1NLZ5auwBrY08/YuqRtmrwsnaEXh98YUpGKnhJG7ncsT24dZMtZqNiFt/hw59cOqDLzmzFsTSq/IbDgrNGpJQ72xNvtbaKf2JHBjeSQDmhtrkeg5EUQuYrGFxwfZFwFrDK1W2tcJS9YcJod+5XrNX7d/9Tsl3qRQ6IO75xX8hhBMIj0wSXsyJ3KIapu0HouJ+xxRds4ILEjOO82eAgHnyF7EhUpJKNt/npfHGeZP4MXlPifA3gktIsvB0EiEXVHke2/3dIzup7YuefHbuTN+sDYMcCLhxpGuSpRCK8rY8HuyUD5Yh+oadJFA+r0cTnyHqnAonOBHQxZIMlUqlHsFl7YsmgwEAx',
+    'client-token': 'AAB9rgGNn2APLxRiaxH+Pt3VXoJpC/s5FkLSJNDajou2QLN+8az8Q/gOYcze5fttek4bnJ0/txJFABUPJj0tEkerddCum+K7EzBmgRcApC/RPnRtALoGH2xk+BJ7DKWqSQZGgWwnFyGqobGPGH8KxkiY17Fqi3o5/tPjJLY4Glr8nggs9bfm7OUGlOG8NwPOpxPLbAWQSqyk8lr/EZ0QrguFPmLER1CICpJEZHzYWp6Qti+EA9p4XN7qs3M+IIiQAI2mV1/V2Iwn5Da0nBZuayAdRM8SRZ3XNs9MAZa6Paqv455jWg1iooDJGNn8NXORyncOPworMwdJqTcMQi0avaanmXmMfEP08/Q=',
     'content-type': 'application/json;charset=UTF-8',
     'origin': 'https://open.spotify.com',
     'pragma': 'no-cache',
@@ -24,45 +25,53 @@ headers = {
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-site',
-    'spotify-app-version': '896000000',
+    'spotify-app-version': '1.2.89.2.g66a2067b',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36',
 }
 
 json_data = {
     'variables': {
-        'uri': 'spotify:artist:1wRPtKGflJrBx9BmLsSwlU',
-        'locale': '',
-        'preReleaseV2': False,
+        'uri': 'spotify:section:0JQ5DAnM3wGh0gz1MXnu3C',
+        'homeEndUserIntegration': 'INTEGRATION_WEB_PLAYER',
+        'timeZone': 'Asia/Calcutta',
+        'sp_t': 'd704ac76-c178-4b8c-921c-df91228c7fd7',
+        'sectionItemsOffset': 0,
+        'sectionItemsLimit': 20,
     },
-    'operationName': 'queryArtistOverview',
+    'operationName': 'homeSection',
     'extensions': {
         'persistedQuery': {
             'version': 1,
-            'sha256Hash': '7f86ff63e38c24973a2842b672abe44c910c1973978dc8a4a0cb648edef34527',
+            'sha256Hash': '23e37f2e58d82d567f27080101d36609009d8c3676457b1086cb0acc55b72a5d',
         },
     },
 }
 
 response = requests.post('https://api-partner.spotify.com/pathfinder/v2/query', headers=headers, json=json_data)
 data=json.loads(response.text)
-print(data)
 artists_data = []
-path = data.get("data", {}).get("albumUnion", {}).get("tracksV2", {}).get("items", [])
 
-for item in path:
-    track = item.get("track", {})
-    artists = track.get("artists", {}).get("items", [])
+sections = data.get("data", {}).get("homeSections", {}).get("sections", [])
 
-    for artist in artists:
-        artists_data.append({
-     
-        "name" : artist.get("profile", {}).get("name"),
-        "uri" : artist.get("uri"),
-        "artist_id" : artist.get("uri").split(":")[-1] if artist.get("uri") else None
-        })
-        
+for section in sections:
+    items = section.get("sectionItems", {}).get("items", [])
     
-print(artists_data)
+    for item in items:
+        artist = item.get("content", {}).get("data", {})
+        profile = artist.get("profile", {})
+        visuals = artist.get("visuals", {})
+        
+        # Get image URL (largest = 640px)
+        sources = visuals.get("avatarImage", {}).get("sources", [])
+        image_url = sources[0].get("url") if sources else None
+        
+        artists_data.append({
+            "name": profile.get("name"),
+             "url": f"https://open.spotify.com/artist/{artist.get('uri', '').split(':')[-1]}",
+            "artist_id": artist.get("uri", "").split(":")[-1],
+            "image_url": image_url
+        })
+
 @app.get("/artists")
 def get_artists():
     return artists_data
